b31d34dcb4c5935569283e4eb06942aac465e77f
m salman a <mohammadalfarisi6@gmail.com>
2025-03-01 17:46:38 +0700

    refactor: Move lspconfig to separate folder

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,52 +1,9 @@
 local servers = { "lua_ls", "ts_ls", "jsonls" }
 
 local on_attach = function(_, buffer)
-	local has_telescope, telescope_builtin = pcall(require, "telescope.builtin")
-	local keymaps = {
-		{ "gd", "<C-]>", desc = "[G]et [D]efinition", remap = true },
-		{ "gD", vim.lsp.buf.declaration, desc = "[G]et [D]eclaration" },
-		{ "gy", vim.lsp.buf.type_definition, desc = "[G]et T[y]pe Definition" },
-		{
-			"gI",
-			function()
-				if has_telescope then
-					telescope_builtin.lsp_implementations()
-				else
-					vim.lsp.buf.implementation()
-				end
-			end,
-			desc = "[G]et [I]mplementations",
-		},
-		{
-			"gr",
-			function()
-				if has_telescope then
-					telescope_builtin.lsp_references()
-				else
-					vim.lsp.buf.references()
-				end
-			end,
-			desc = "[G]et [R]eferences",
-		},
-		{
-			"<leader>fs",
-			function()
-				if has_telescope then
-					telescope_builtin.lsp_document_symbols()
-				else
-					vim.lsp.buf.document_symbol()
-				end
-			end,
-			desc = "[F]ind [S]ymbols",
-		},
-		{ "<leader>ca", vim.lsp.buf.code_action, desc = "[C]ode [A]ction" },
-		{ "<leader>rn", vim.lsp.buf.rename, desc = "[R]e[n]ame" },
-	}
+	local keymaps = require("plugins.lsp.keymaps")
 
-	-- For other defaults use:
-	-- :help lsp-defaults
-
-	for _, keys in ipairs(keymaps) do
+	for _, keys in ipairs(keymaps.get()) do
 		local lhs = keys[1]
 		local rhs = keys[2]
 		keys[1] = nil
