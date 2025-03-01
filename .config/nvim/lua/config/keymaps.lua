5110234fdcbb7fe39c18733f4590495be6e9e253
m salman a <mohammadalfarisi6@gmail.com>
2025-03-01 21:24:45 +0700

    Set keymaps to switch lines with j and k

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,15 @@
 local opts = { silent = true }
 
 -- Resize
-vim.keymap.set("n", "<M-h>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<M-j>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<M-k>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<M-l>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<M-h>", ":vertical resize -2<cr>", opts)
+vim.keymap.set("n", "<M-j>", ":resize +2<cr>", opts)
+vim.keymap.set("n", "<M-k>", ":resize -2<cr>", opts)
+vim.keymap.set("n", "<M-l>", ":vertical resize +2<cr>", opts)
 
 -- Buffers
-vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bnext<cr>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<cr>", opts)
 
 -- Swap lines
-vim.keymap.set("n", "<M-Up>", ":m-2<CR>==", opts)
-vim.keymap.set("n", "<M-Down>", ":m+<CR>==", opts)
-vim.keymap.set("x", "<M-Up>", ":m-2<CR>gv=gv", opts)
-vim.keymap.set("x", "<M-Down>", ":m'>+<CR>gv=gv", opts)
+vim.keymap.set("x", "<M-j>", ":m'>+<CR>gv=gv", opts)
+vim.keymap.set("x", "<M-k>", ":m-2<CR>gv=gv", opts)
