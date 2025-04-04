57afba101cf6c3a0b57cdcfb6e2bb3df909a113d
m salman a <mohammadalfarisi6@gmail.com>
2025-04-04 21:08:34 +0700

    Migrate to nvim-ts-context-commentstring to ts-comments plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -44,18 +44,9 @@ return {
 	},
 
 	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		config = function()
-			require("ts_context_commentstring").setup({
-				enable_autocmd = false,
-			})
-
-			local get_option = vim.filetype.get_option
-			vim.filetype.get_option = function(filetype, option)
-				return option == "commentstring"
-						and require("ts_context_commentstring.internal").calculate_commentstring()
-					or get_option(filetype, option)
-			end
-		end,
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
 	},
 }
