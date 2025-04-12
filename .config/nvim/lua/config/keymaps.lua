24185abb3f6d5cabb7e29ba59845ce9753a47885
m salman a <mohammadalfarisi6@gmail.com>
2025-04-12 22:21:11 +0700

    Add keymaps for sourcing files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,3 +13,8 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 -- Swap lines
 vim.keymap.set("x", "<M-j>", ":m'>+<CR>gv=gv", opts)
 vim.keymap.set("x", "<M-k>", ":m-2<CR>gv=gv", opts)
+
+-- Source files
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
