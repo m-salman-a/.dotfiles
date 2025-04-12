a58a2d0e8eaaeeb03f2ee09f7685d7c4dded9b33
m salman a <mohammadalfarisi6@gmail.com>
2025-04-13 00:15:26 +0700

    Add toggle command for format on save

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -5,6 +5,8 @@ return {
 			local conform = require("conform")
 
 			conform.setup({
+				event = { "BufWritePre" },
+				cmd = { "ConformInfo" },
 				formatters_by_ft = {
 					lua = { "stylua" },
 					javascript = { "prettier" },
@@ -12,10 +14,32 @@ return {
 					typescript = { "prettier" },
 					typescriptreact = { "prettier" },
 				},
-				format_on_save = {
-					timeout_ms = 1000,
-					lsp_format = "fallback",
-				},
+				format_on_save = function(bufnr)
+					-- Disable with a global or buffer-local variable
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+					return { timeout_ms = 500, lsp_format = "fallback" }
+				end,
+			})
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 
 			vim.keymap.set("n", "<leader>fm", conform.format, {
