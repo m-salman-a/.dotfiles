2f280951a44ef6331a360adbaafddb8f98ad8f74
m salman a <mohammadalfarisi6@gmail.com>
2025-04-18 21:02:06 +0700

    Disable default_file_explorer on Oil

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -140,7 +140,9 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			default_file_explorer = false,
+		},
 		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 		lazy = false,
 	},
