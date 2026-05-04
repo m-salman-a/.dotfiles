1622ba77819c4f3d8db6d188a3bdc03cd0fcd309
m-salman-a <mohammadalfarisi6@gmail.com>
2026-05-04 12:05:16 +0700

    Remove flutter tools color highlight

--- .config/nvim/after/plugin/flutter-tools.lua
+++ .config/nvim/after/plugin/flutter-tools.lua
@@ -3,13 +3,6 @@ local config = require("config.lsp")
 require("flutter-tools").setup({
   fvm = true,
   lsp = {
-    color = {
-      enabled = true,
-      foreground = true,
-      background = true,
-      virtual_text = true,
-      virtual_text_str = "■",
-    },
     on_attach = config.on_attach,
   },
 })
